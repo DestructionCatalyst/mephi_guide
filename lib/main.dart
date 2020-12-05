b358269d8fe4ae7bb9b6defd59645b365b0ee8ea
Владислав Комза <komza.vlad@ya.ru>
2020-12-05 21:34:48 +0300

    Make reminders tab stateful

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:http/io_client.dart';
 import 'package:mephi_guide/data/http_fetch.dart';
-import 'file:///C:/Users/User/AndroidStudioProjects/mephi_guide/lib/reminders/rem.dart';
+import 'package:mephi_guide/reminders/rem.dart';
 
 
 
@@ -78,31 +78,12 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      //appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        //title: Text(widget.title),
-      //),
       body: Center(
-      child: MobileWidget2()//138
+      child: RemindersTab()
         /*
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
--- lib/reminders/rem.dart
+++ lib/reminders/rem.dart
@@ -10,13 +10,18 @@ import 'circle_status_widget.dart';
 import '../group_manager.dart';
 import '../rounded_button.dart';
 
-class MobileWidget2 extends StatelessWidget {
+class RemindersTab extends StatefulWidget {
+  @override
+  _RemindersTabState createState() => _RemindersTabState();
+}
+
+class _RemindersTabState extends State<RemindersTab> {
+
   final GroupManager reminderButtonsGroup = new GroupManager();
   final RemindersBloc bloc = new RemindersBloc();
 
   @override
   Widget build(BuildContext context) {
-    // Figma Flutter Generator MobileWidget - FRAME
     return Container(
         width: 360,
         height: 640,
@@ -51,312 +56,319 @@ class MobileWidget2 extends StatelessWidget {
     );
   }
 
+
+  @override
+  void dispose() {
+    bloc.dispose();
+    super.dispose();
+  }
+
   Positioned buildLineAndDotNearTitle() {
     return Positioned(
-                top: 240,
-                left: 123,
-                child: Container(
-                    width: 43,
-                    height: 4,
+        top: 240,
+        left: 123,
+        child: Container(
+            width: 43,
+            height: 4,
 
-                    child: Stack(
-                        children: <Widget>[
-                          Positioned(
-                              top: 0.666656494140625,
-                              left: 0,
-                              child: Container(
-                                  width: 25,
-                                  height: 2.666656494140625,
-                                  decoration: BoxDecoration(
-                                    borderRadius : BorderRadius.only(
-                                      topLeft: Radius.circular(3),
-                                      topRight: Radius.circular(3),
-                                      bottomLeft: Radius.circular(3),
-                                      bottomRight: Radius.circular(3),
-                                    ),
-                                    color : Color.fromRGBO(76, 207, 211, 1),
-                                  )
-                              )
-                          ),Positioned(
-                              top: 0,
-                              left: 39,
-                              child: Container(
-                                  width: 4,
-                                  height: 4,
-                                  decoration: BoxDecoration(
-                                    borderRadius : BorderRadius.only(
-                                      topLeft: Radius.circular(3),
-                                      topRight: Radius.circular(3),
-                                      bottomLeft: Radius.circular(3),
-                                      bottomRight: Radius.circular(3),
-                                    ),
-                                    color : Color.fromRGBO(185, 191, 202, 1),
-                                  )
-                              )
-                          ),
-                        ]
-                    )
-                )
-            );
+            child: Stack(
+                children: <Widget>[
+                  Positioned(
+                      top: 0.666656494140625,
+                      left: 0,
+                      child: Container(
+                          width: 25,
+                          height: 2.666656494140625,
+                          decoration: BoxDecoration(
+                            borderRadius : BorderRadius.only(
+                              topLeft: Radius.circular(3),
+                              topRight: Radius.circular(3),
+                              bottomLeft: Radius.circular(3),
+                              bottomRight: Radius.circular(3),
+                            ),
+                            color : Color.fromRGBO(76, 207, 211, 1),
+                          )
+                      )
+                  ),Positioned(
+                      top: 0,
+                      left: 39,
+                      child: Container(
+                          width: 4,
+                          height: 4,
+                          decoration: BoxDecoration(
+                            borderRadius : BorderRadius.only(
+                              topLeft: Radius.circular(3),
+                              topRight: Radius.circular(3),
+                              bottomLeft: Radius.circular(3),
+                              bottomRight: Radius.circular(3),
+                            ),
+                            color : Color.fromRGBO(185, 191, 202, 1),
+                          )
+                      )
+                  ),
+                ]
+            )
+        )
+    );
   }
 
   Positioned buildButtons() {
     return Positioned(
-                top: 0,
-                left: -0.5,
-                child: Container(
-                    width: 361,
-                    height: 640,
+        top: 0,
+        left: -0.5,
+        child: Container(
+            width: 361,
+            height: 640,
 
-                    child: Stack(
-                        children: <Widget>[
-                          //Buttons
-                          Positioned(
-                              top: 277,
-                              left: 16.5,
-                              child: Container(
-                                  width: 328,
-                                  height: 38,
+            child: Stack(
+                children: <Widget>[
+                  //Buttons
+                  Positioned(
+                      top: 277,
+                      left: 16.5,
+                      child: Container(
+                          width: 328,
+                          height: 38,
 
-                                  child: Stack(
-                                      children: <Widget>[
-                                        Positioned(
-                                          top: 0,
-                                          left: 0,
-                                          child: RoundedToggleButton(
-                                            width: 101,
-                                            height: 38,
-                                            text: "Все",
-                                            defaultPressed: true,
-                                            onTap: (){},
-                                            buttonGroup: reminderButtonsGroup,
-                                          ),
-                                        ),
-                                        Positioned(
-                                          top: 0,
-                                          left: 115,
-                                          child: RoundedToggleButton(
-                                              width: 213,
-                                              height: 38,
-                                              text: "Невыполненные",
-                                              defaultPressed: false,
-                                              onTap: (){},
-                                              buttonGroup: reminderButtonsGroup,
-                                          ),
-                                        ),
-                                      ]
-                                  )
-                              )
-                          ),
-                          //There was smth with rectanglepath
-                        ]
-                    )
-                )
-            );
+                          child: Stack(
+                              children: <Widget>[
+                                Positioned(
+                                  top: 0,
+                                  left: 0,
+                                  child: RoundedToggleButton(
+                                    width: 101,
+                                    height: 38,
+                                    text: "Все",
+                                    defaultPressed: true,
+                                    onTap: (){},
+                                    buttonGroup: reminderButtonsGroup,
+                                  ),
+                                ),
+                                Positioned(
+                                  top: 0,
+                                  left: 115,
+                                  child: RoundedToggleButton(
+                                    width: 213,
+                                    height: 38,
+                                    text: "Невыполненные",
+                                    defaultPressed: false,
+                                    onTap: (){},
+                                    buttonGroup: reminderButtonsGroup,
+                                  ),
+                                ),
+                              ]
+                          )
+                      )
+                  ),
+                  //There was smth with rectanglepath
+                ]
+            )
+        )
+    );
   }
 
   Positioned buildAnotherDotNearTitle() {
     return Positioned(
-                top: 240,
-                left: 183,
-                child: Container(
-                    width: 4,
-                    height: 4,
-                    decoration: BoxDecoration(
-                      borderRadius : BorderRadius.only(
-                        topLeft: Radius.circular(3),
-                        topRight: Radius.circular(3),
-                        bottomLeft: Radius.circular(3),
-                        bottomRight: Radius.circular(3),
-                      ),
-                      color : Color.fromRGBO(185, 191, 202, 1),
-                    )
-                )
-            );
+        top: 240,
+        left: 183,
+        child: Container(
+            width: 4,
+            height: 4,
+            decoration: BoxDecoration(
+              borderRadius : BorderRadius.only(
+                topLeft: Radius.circular(3),
+                topRight: Radius.circular(3),
+                bottomLeft: Radius.circular(3),
+                bottomRight: Radius.circular(3),
+              ),
+              color : Color.fromRGBO(185, 191, 202, 1),
+            )
+        )
+    );
   }
 
   Positioned buildTabTitle() {
     return Positioned(
-                top: 228,
-                left: 16,
-                child: Text('Памятка', textAlign: TextAlign.left, style: TextStyle(
-                    color: Color.fromRGBO(46, 31, 112, 1),
-                    fontFamily: 'Roboto',
-                    fontSize: 20,
-                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.bold,
-                    height: 1
-                ),)
-            );
+        top: 228,
+        left: 16,
+        child: Text('Памятка', textAlign: TextAlign.left, style: TextStyle(
+            color: Color.fromRGBO(46, 31, 112, 1),
+            fontFamily: 'Roboto',
+            fontSize: 20,
+            letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+            fontWeight: FontWeight.bold,
+            height: 1
+        ),)
+    );
   }
 
   Positioned buildCircleStatusWidget() {
     return Positioned(
-              top: 70,
-              left: 0,
-              child: CircleStatusWidget(),
-            );
+      top: 70,
+      left: 0,
+      child: CircleStatusWidget(),
+    );
   }
 
   Positioned buildList() {
     return Positioned(
-                top: 331,
-                left: 0,
+        top: 331,
+        left: 0,
 
-                child: Container(
-                  width: 360,
-                  height: 309,
-                    child: RemindersList(bloc: bloc),
-                )
-            );
+        child: Container(
+          width: 360,
+          height: 309,
+          child: RemindersList(bloc: bloc),
+        )
+    );
   }
 
   Positioned buildWhiteFilling() {
     return Positioned(
-                top: -7,
-                left: -8,
-                child: Container(
-                    width: 373,
-                    height: 228,
-                    decoration: BoxDecoration(
-                      color : Color.fromRGBO(255, 255, 255, 1),
-                    )
-                )
-            );
+        top: -7,
+        left: -8,
+        child: Container(
+            width: 373,
+            height: 228,
+            decoration: BoxDecoration(
+              color : Color.fromRGBO(255, 255, 255, 1),
+            )
+        )
+    );
   }
 
   Positioned buildWigglyRectangle() {
     return Positioned(
-                top: 0,
-                left: 0,
-                child: SvgPicture.asset(
-                    'assets/images/Placeholder.svg',
-                    semanticsLabel: 'Placeholder'
-                )
-            );
+        top: 0,
+        left: 0,
+        child: SvgPicture.asset(
+            'assets/images/Placeholder.svg',
+            semanticsLabel: 'Placeholder'
+        )
+    );
   }
 
   Positioned buildGearButton() {
     return Positioned(
-                top: 34,
-                left: 310,
+        top: 34,
+        left: 310,
 
-                child: CircleButton(
-                  diameter: 40,
-                  color: Color.fromRGBO(255, 255, 255, 0.3),
+        child: CircleButton(
+          diameter: 40,
+          color: Color.fromRGBO(255, 255, 255, 0.3),
 
-                  icon: SvgPicture.asset(
-                      'assets/images/vector.svg',
-                      semanticsLabel: 'vector'
-                  ),
-                  iconTop: 12,
-                  iconLeft: 12,
-                )
-            );
+          icon: SvgPicture.asset(
+              'assets/images/vector.svg',
+              semanticsLabel: 'vector'
+          ),
+          iconTop: 12,
+          iconLeft: 12,
+        )
+    );
   }
 
   Positioned buildPenButton() {
     return Positioned(
-                top: 34,
-                left: 254,
+      top: 34,
+      left: 254,
 
-                child: CircleButton(
-                  diameter: 40,
-                  color: Color.fromRGBO(76, 207, 211, 1),
+      child: CircleButton(
+        diameter: 40,
+        color: Color.fromRGBO(76, 207, 211, 1),
 
-                  icon: SvgPicture.asset(
-                      'assets/images/vector_pen.svg',
-                      semanticsLabel: 'vector_pen'
-                  ),
-                  iconTop: 12,
-                  iconLeft: 12,
-                  onTap: ()=>print("Tapped!"),
-                ),
+        icon: SvgPicture.asset(
+            'assets/images/vector_pen.svg',
+            semanticsLabel: 'vector_pen'
+        ),
+        iconTop: 12,
+        iconLeft: 12,
+        onTap: ()=>print("Tapped!"),
+      ),
 
-            );
+    );
   }
 
   Positioned buildCompletedText() {
     return Positioned(
-                top: 149,
-                left: 254,
-                child: Text(' >/ выполнено', textAlign: TextAlign.left, style: TextStyle(
-                    color: Color.fromRGBO(185, 192, 202, 1),
-                    fontFamily: 'Roboto',
-                    fontSize: 12,
-                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1
-                  ),
-                )
-            );
+        top: 149,
+        left: 254,
+        child: Text(' >/ выполнено', textAlign: TextAlign.left, style: TextStyle(
+            color: Color.fromRGBO(185, 192, 202, 1),
+            fontFamily: 'Roboto',
+            fontSize: 12,
+            letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+            fontWeight: FontWeight.normal,
+            height: 1
+        ),
+        )
+    );
   }
 
   Positioned buildPercentageText() {
     return Positioned(
-                top: 142,
-                left: 161,
-                child: Text('75%', textAlign: TextAlign.left, style: TextStyle(//TODO dynamic change
-                    color: Color.fromRGBO(76, 207, 211, 1),
-                    fontFamily: 'Roboto',
-                    fontSize: 50,
-                    letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
-                    fontWeight: FontWeight.normal,
-                    height: 1
-                ),)
-            );
+        top: 142,
+        left: 161,
+        child: Text('75%', textAlign: TextAlign.left, style: TextStyle(//TODO dynamic change
+            color: Color.fromRGBO(76, 207, 211, 1),
+            fontFamily: 'Roboto',
+            fontSize: 50,
+            letterSpacing: 0 /*percentages not used in flutter. defaulting to zero*/,
+            fontWeight: FontWeight.normal,
+            height: 1
+        ),)
+    );
   }
 
   Positioned buildFancyLines() {
     return Positioned(
-                top: -7,
-                left: 44,
-                child: SvgPicture.asset(
-                    'assets/images/vector1.svg',
-                    semanticsLabel: 'vector1'
-                )
-                //Circles
-            );
+        top: -7,
+        left: 44,
+        child: SvgPicture.asset(
+            'assets/images/vector1.svg',
+            semanticsLabel: 'vector1'
+        )
+      //Circles
+    );
   }
 
   Positioned buildBell() {
     return Positioned(
-                top: 130,
-                left: 49,
-                child: Container(
-                    width: 42,
-                    height: 42,
-                    decoration: BoxDecoration(
-                      color : Color.fromRGBO(255, 255, 255, 1),
-                    ),
-                    child: Stack(
-                        children: <Widget>[
-                          Positioned(
-                              top: 0,
-                              left: 0,
-                              child: SvgPicture.asset(
-                                  'assets/images/Notification.svg',
-                                  semanticsLabel: 'Notification'
-                              )
-                          ),
-                        ]
-                    )
-                )
-            );
+        top: 130,
+        left: 49,
+        child: Container(
+            width: 42,
+            height: 42,
+            decoration: BoxDecoration(
+              color : Color.fromRGBO(255, 255, 255, 1),
+            ),
+            child: Stack(
+                children: <Widget>[
+                  Positioned(
+                      top: 0,
+                      left: 0,
+                      child: SvgPicture.asset(
+                          'assets/images/Notification.svg',
+                          semanticsLabel: 'Notification'
+                      )
+                  ),
+                ]
+            )
+        )
+    );
   }
 
   Positioned buildMephiTitle() {
     return Positioned(
-                top: 32,
-                left: 16,
-                child: Text('MEPhI >/', textAlign: TextAlign.left, style: TextStyle(
-                    color: Color.fromRGBO(255, 255, 255, 1),
-                    fontFamily: 'Roboto',
-                    fontSize: 35,
-                    letterSpacing: -1,
-                    fontWeight: FontWeight.normal,
-                    height: 1.3714285714285714
-                ),)
-            );
+        top: 32,
+        left: 16,
+        child: Text('MEPhI >/', textAlign: TextAlign.left, style: TextStyle(
+            color: Color.fromRGBO(255, 255, 255, 1),
+            fontFamily: 'Roboto',
+            fontSize: 35,
+            letterSpacing: -1,
+            fontWeight: FontWeight.normal,
+            height: 1.3714285714285714
+        ),)
+    );
   }
 }
