94fef74c18f51d1b959d198bc7e43d8aaa154cef
Владислав Комза <komza.vlad@ya.ru>
2021-04-27 17:53:26 +0300

    Finish big query

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,8 @@ void main() async{
   
   List<Map<String, dynamic>> res;
   
-  //res = await DBProvider.rawQuery("SELECT * FROM lessons JOIN teachers_lessons ON lessons.id = teachers_lessons.idLesson JOIN teachers ON teachers_lessons.idTeacher = teachers.id");
+  //res = await DBProvider.rawQuery("SELECT * FROM lessons JOIN teachers_lessons
+  // ON lessons.id = teachers_lessons.idLesson JOIN teachers ON teachers_lessons.idTeacher = teachers.id");
 
   var rawGroup = await DBProvider.rawQuery("SELECT value FROM utility WHERE key = \"currentGroupID\"");
 
@@ -50,29 +51,34 @@ void main() async{
       "),"
       "cte_teacher AS ("
       "  SELECT id, subjectName, teacherName, type, weekOdd, startTime, endTime "
-      "FROM cte_subject "
+      "  FROM cte_subject "
       "  JOIN teachers_lessons ON id = idLesson "
       "  JOIN ("
       "    SELECT id as teacherId, name as teacherName FROM teachers "
       "  ) ON idTeacher = teacherId"
       "),"
       "cte_groups AS ("
-      " SELECT id, subjectName, teacherName, groupName, type, weekOdd, startTime, endTime "
-      "FROM cte_teacher "
+      "  SELECT id, subjectName, teacherName, groupName, type, weekOdd, startTime, "
+      "  endTime FROM cte_teacher "
       "  JOIN groups_lessons ON id = idLesson "
       "  JOIN ("
       "    SELECT id as groupId, name as groupName FROM groups "
       "  ) ON idGroup = groupId"
       "),"
       "cte_places AS ("
-      " SELECT id, subjectName, teacherName, groupName, placeName, type, weekOdd, startTime, endTime "
-      "FROM cte_groups "
+      "  SELECT id, subjectName, teacherName, groupName, placeName, type, weekOdd, "
+      "  startTime, endTime FROM cte_groups "
       "  JOIN places_lessons ON id = idLesson "
       "  JOIN ("
       "    SELECT id as placeId, name as placeName FROM places "
       "  ) ON idPlace = placeId"
       ")"
-      "SELECT * FROM cte_places");
+      "SELECT id, subjectName, "
+      " GROUP_CONCAT(DISTINCT teacherName) AS teachers,"
+      " GROUP_CONCAT(DISTINCT groupName) AS groups,"
+      " GROUP_CONCAT(DISTINCT placeName) AS places, "
+      " type, weekOdd, startTime, endTime "
+      "FROM cte_places GROUP BY id");
 
   print(res);
 
